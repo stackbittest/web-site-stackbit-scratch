58d7a13cd27a36a44b19303daf7a862bd2a3873e
My name <example@example.com>
2020-12-22 11:55:32 -0600

    Left out a line

--- src/templates/dessert.js
+++ src/templates/dessert.js
@@ -1,23 +1,20 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby";
 
 export default function Dessert({ pageContext }) {
-  return (
-    <div>
-	  {pageContext.frontmatter.message}
-    </div>
-  )
+  return <div>{pageContext.frontmatter.message}</div>;
 }
 
 // Per Simon, this minimal GraphQL query ensures that when 'gatsby develop' is running
 // any changes to content files affecting this page are refreshed in browser
 export const query = graphql`
-query {
-  allSitePage {
-    edges {
-      node {
-        id
+  query {
+    allSitePage {
+      edges {
+        node {
+          id
+        }
       }
     }
   }
-}
 `;
