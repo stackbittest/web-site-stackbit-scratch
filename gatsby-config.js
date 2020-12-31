a6cc866e4b4473a181f4bf461403e7509f7c8866
My name <example@example.com>
2020-12-31 08:35:56 -0600

    Neat I did not break local Gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `pages`,
-                path: `${__dirname}/src/minnesotatreats`,
+                path: `${__dirname}/src/pages/minnesotatreats`,
             },
         },
 		`gatsby-transformer-remark`
